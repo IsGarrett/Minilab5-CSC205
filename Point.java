7ffe1d05eedac52c02a5b66bd05f87bbc9149475
IsGarrett <166757512+IsGarrett@users.noreply.github.com>
2024-10-02 11:14:21 -0700

    done

--- Point.java
+++ Point.java
@@ -1,9 +1,18 @@
+/* CSC 205: <Class 26063> / <Online>
+Minilab: <Minilab 5>
+Author(s): <Garrett Lambert> & <gar2161746>
+
+Description: The purpose of this code is to provide methods to perform various functions utilizing x and y coordinates. 
+The code implements the PointInterface class and provides basic operations for working with 2D points.
+*/
+
 public class Point implements PointInterface {
 
+    //----Data-----
     private int x;
     private int y;
 
-
+    //Constructor
     public Point() {
 
         this.x = 2;
@@ -11,6 +20,7 @@ public class Point implements PointInterface {
 
     }
 
+    //Parameterized Constructor
     public Point(int theX, int theY) {
 
         this.x = theX;
@@ -18,6 +28,7 @@ public class Point implements PointInterface {
 
     }
 
+    //Copy Constructor -- Verifies if value is null and ifso, returns error 
     public Point(Point other) {
         
         if (other == null) {
@@ -29,17 +40,19 @@ public class Point implements PointInterface {
 
     }
 
+    //prints out the (x, y) coordinates in correct formatting
     public String toString() {
 
         return "(" + this.x + "," + this.y + ")";
 
     }
 
+    //uses Math.util Math.hypot to perform calculations of how far each point is from another
     @Override
     public double distanceTo(Point otherPoint) {
         
         if ( otherPoint == null ) {
-            throw new UnsupportedOperationException("Unimplemented method 'distanceTo'");
+            throw new IllegalArgumentException("Unimplemented method 'distanceTo'");
         }
 
         double distance = Math.hypot(this.x - otherPoint.x , this.y - otherPoint.y);
@@ -48,6 +61,7 @@ public class Point implements PointInterface {
 
     }
 
+    //checks to find if inital point is equal to otherPoint
     @Override
     public boolean equals(Object obj) {
 
@@ -66,24 +80,34 @@ public class Point implements PointInterface {
 
     }
     
-
+    //checks to see if point is in correct quadrant, throws error if not. 
     @Override
     public boolean inQuadrant(int quadrant) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'inQuadrant'");
+        
+        if (quadrant < 1 || quadrant > 4) {
+            throw new IllegalArgumentException("Unimplemented method 'inQuadrant'");
+        }
+        if (quadrant == 1) {
+            return x > 0 && y > 0;
+        } else if (quadrant == 2) {
+            return x < 0 && y > 0;
+        } else if (quadrant == 3) {
+            return x < 0 && y < 0;
+        } else if (quadrant == 4) {
+            return x > 0 && y < 0;
+        }
+        return false;
     }
 
+
+    //translate the points by adding the xMove and yMove to the initial x and y.
     @Override
     public void translate(int xMove, int yMove) {
-        // TODO Auto-generated method stub
-
-        //not sure here
-        throw new UnsupportedOperationException("Unimplemented method 'translate'");
+        
+        this.x += xMove;
+        this.y += yMove;
+        
     }
 
-    
-
-
-
-    
+   
 }
